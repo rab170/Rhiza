4924177280115f33ee20208d3758ecfbaec8d2c6
Rob Brown <rab170@pitt.edu>
2014-04-15 00:25:32 -0400

    functionality to recognize recently modified files added

--- google_sync.py
+++ google_sync.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 """
-********************************
-Rhiza Screening -- Google Sync *
-********************************
-** Rob Brown; rab170@pitt.edu **
-********************************
+**********************************
+* Rhiza Screening -- Google Sync *
+**********************************
+*** Rob Brown; rab170@pitt.edu ***
+**********************************
 
 TL;DR --this program syncs ~/GoogleDrive/* with the google account identified in ~/login.txt
 
@@ -16,49 +15,74 @@ This program operates in unison with a CRON job, which watches ~/GoogleDrive/* f
 **Note that you will not find auto-removal functionality, nor upstream changes being pulled to the local ~/GoogleDrive directory
 
 """
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import gdata.docs.data
 import gdata.docs.client
+import os, time
 
-import os.path, time
-
+GOOGLE_DRIVE_PATH=os.path.expanduser("~/Dropbox")
+LAST_UPDATE_PATH='.last_update'     #hidden file storing time of last execution, hidden according to UNIX filename conventions (preceding .)
 
 def get_last_update():
-    if not os.path.isfile('last_update.txt'): return None
-    f =open('last_update.txt')
+    if not os.path.isfile(LAST_UPDATE_PATH): return None
+    f =open(LAST_UPDATE_PATH)
     return time.strptime(f.readline(), '%Y-%m-%d %H:%M:%S')
 
 def change_last_update():
-    f =open('last_update.txt', 'w+')
-    time_str = time.strftime('%Y-%m-%d %H:%M:%S')
+    f =open(LAST_UPDATE_PATH, 'w+')
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
     f.write(time_str)
 
-def connect_client():
+def connect_client():   #creates connection client for user identified in ~/login.txt
     global client
     LOGIN_PATH=os.path.expanduser("~/login.txt")
     login_file = open(LOGIN_PATH);
     USER=login_file.readline()
     PASS=login_file.readline()
+
     client = gdata.docs.client.DocsClient(source='google_sync.py')
     client.api_version = "3"
     client.ssl = True
     client.ClientLogin(USER, PASS, client.source)
 
 
+def get_modifications(LAST_UPDATE):
+    """
+        get_modifications(LAST_UPDATE) returns a list of absolute paths in ~/GoogleDrive/* that have been modified since LAST_UPDATE.
+        LAST_UPDATE is a struct_time object representing the last time the script updated files in Google Drive -- see get_last_update()
 
-if __name__ == "__main__":
-    last_update = get_last_update()
-    change_last_update()
-    connect_client()
+        all times are UTC
+    """
+    to_update = []
+    for root, dirs, files in os.walk(GOOGLE_DRIVE_PATH):
+        #if not time.gmtime(os.stat(root).st_mtime) >= LAST_UPDATE:     #Unfortunately, last modification time of directories is only effected by adding/removing/renaming subdirectories (not files).
+            #del dirs[:]                                                #can't avoid full-depth recursion (without maintaining logs)
+        for file_name in files:
+            abs_path = root + "/" +  file_name
+            if time.gmtime(os.stat(abs_path).st_mtime) >= LAST_UPDATE:      #check files at each depth, append to_update if they have been modified since LAST_UPDATE
+                to_update.append(abs_path)
+    return to_update
 
+def sync(paths):
+    #TODO build directory structure,
+    #TODO check if file exists, if so overwrite with new revision, else create
+    for path in paths:
+        print path
 
+def sync_modifications():
     filePath = "/home/rob/SPRING_2014.txt"
     newResource = gdata.docs.data.Resource(filePath, os.path.basename(filePath))
     media = gdata.data.MediaSource()
-    media.SetFileHandle(filePath, 'set/forget')     #second argument is for intern google useage. Examples are 'image/jpeg' or 'text/plain'. Serverside, this is extrapolated from the file extension. Thus, set and forget.
+    media.SetFileHandle(filePath, 'set/forget')     #'set/forget' is a serverside parameter for file type identifaction. Examples are 'image/jpeg' or 'text/plain'. Google fills in the blanks based on file extensions. Thus, set and forget.
     newDocument = client.CreateResource(newResource, create_uri=gdata.docs.client.RESOURCE_UPLOAD_URI, media=media)
 
-
+if __name__ == "__main__":
+    LAST_UPDATE = get_last_update()
+    change_last_update()
+    connect_client()
+    paths = get_modifications(LAST_UPDATE)
+    sync(paths)
 
 
 
